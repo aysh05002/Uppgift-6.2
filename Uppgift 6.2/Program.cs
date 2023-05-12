583f35346d9d00fd583d082e111874b80255b836
Admin <Admin@TEB123008>
2023-05-12 08:49:38 +0200

    Add project files.

---
+++ Uppgift 6.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class program
+{
+    static int Störst(int tal1, int tal2)
+    {
+        int x = 0;
+        if (tal1 > tal2)
+        {
+            x = tal1;
+        }
+        else if (tal2 > tal1)
+        {
+            x = tal2;
+        }
+        else if (tal1 == tal2)
+        {
+            x = tal1;
+        }
+        return x;
+    }
+}
